4c5d2dffb12ef1402af416d6ac900f1189ec18a5
Diego Castillo <88866352+diegocastillosaldana@users.noreply.github.com>
2022-08-17 10:07:36 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,7 +11,6 @@ $(document).ready(function (argument) {
 	collapsible.forEach(element => {
 		element.addEventListener('click', () => {
 			element.querySelector('.faq-answer').classList.toggle('open')
-			element.querySelector('.question').classList.toggle('active')
 		})
 	})
 })
