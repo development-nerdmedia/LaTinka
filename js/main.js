f85328acc1136d95d4c4367ed7dfa1d144a4c03c
Diego Castillo <88866352+diegocastillosaldana@users.noreply.github.com>
2022-08-17 14:58:56 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,16 +1,78 @@
-$(document).ready(function (argument) {
-    AOS.init();
-    if ($('.img-tienda').length > 0) {
-		$(".img-tienda").stick_in_parent({
-			offset_top: 180
-		});
-	}   
+AOS.init();
 
-	let collapsible = document.querySelectorAll('.faq-container')
+var confettiSettings = { target: 'my-canvas' };
+var confetti = new ConfettiGenerator(confettiSettings);
+confetti.render();
 
-	collapsible.forEach(element => {
-		element.addEventListener('click', () => {
-			element.querySelector('.faq-answer').classList.toggle('open')
-		})
-	})
-})
+const menuItems = document.querySelectorAll(
+	'.navigation ul li a[href^="#"]'
+);
+
+function getScrollTopByHref(element) {
+	const id = element.getAttribute("href");
+	return document.querySelector(id).offsetTop;
+}
+
+function scrollToPosition(to) {
+	smoothScrollTo(0, to);
+}
+
+function scrollToIdOnClick(event) {
+	event.preventDefault();
+	const to = getScrollTopByHref(event.currentTarget) - 80;
+	scrollToPosition(to);
+}
+
+menuItems.forEach((item) => {
+	item.addEventListener("click", scrollToIdOnClick);
+});
+function smoothScrollTo(endX, endY, duration) {
+	const startX = window.scrollX || window.pageXOffset;
+	const startY = window.scrollY || window.pageYOffset;
+	const distanceX = endX - startX;
+	const distanceY = endY - startY;
+	const startTime = new Date().getTime();
+
+	duration = typeof duration !== "undefined" ? duration : 600;
+
+	const easeInOutQuart = (time, from, distance, duration) => {
+		if ((time /= duration / 2) < 1)
+			return (distance / 2) * time * time * time * time + from;
+		return (
+			(-distance / 2) * ((time -= 2) * time * time * time - 2) + from
+		);
+	};
+
+	const timer = setInterval(() => {
+		const time = new Date().getTime() - startTime;
+		const newX = easeInOutQuart(time, startX, distanceX, duration);
+		const newY = easeInOutQuart(time, startY, distanceY, duration);
+		if (time >= duration) {
+			clearInterval(timer);
+		}
+		window.scroll(newX, newY);
+	}, 1000 / 60); // 60 fps
+}
+
+if ($(".img-tienda").length > 0) {
+	$(".img-tienda").stick_in_parent({
+		offset_top: 180,
+	});
+}
+
+let collapsible = document.querySelectorAll(".faq-container");
+let answer = document.querySelectorAll(".faq-answer");
+
+function answerOpen() {
+	for (let i = 0; i < answer.length; i++) {
+		answer[i].classList.remove("open");
+	}
+}
+
+collapsible.forEach((element) => {
+	element.addEventListener("click", () => {
+		answerOpen();
+		element.querySelector(".faq-answer").classList.add("open");
+	});
+});
+
